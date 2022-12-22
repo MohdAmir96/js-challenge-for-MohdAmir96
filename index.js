ef2ac4f61897612ef9a9afe65fb289e19d8af984
MohdAmir96 <110561832+MohdAmir96@users.noreply.github.com>
2022-12-22 21:37:47 +0530

    final

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 let globalIsSubmit = false;
 
+function resetFields() {
+  document.querySelector("form").reset();
+
+  document.getElementById("first-name-valid").style.display = "none";
+
+  document.getElementById("last-name-valid").style.display = "none";
+  document.getElementById("phone-valid").style.display = "none";
+  document.getElementById("username-valid").style.display = "none";
+  document.getElementById("email-valid").style.display = "none";
+  document.getElementById("address-valid").style.display = "none";
+  document.getElementById("phone-valid").style.display = "none";
+  document.getElementById("website-valid").style.display = "none";
+  document.getElementById("company-valid").style.display = "none";
+}
+
 function validate(isSubmit) {
   if (isSubmit) {
     globalIsSubmit = true;
@@ -14,9 +29,44 @@ function validate(isSubmit) {
   let website = document.getElementById("website").value;
   let company = document.getElementById("company").value;
 
-  console.log(firstName);
+  // alphanumeric check function
+  function isAlphaNumeric(str) {
+    var code, i, len;
+
+    for (i = 0, len = str.length; i < len; i++) {
+      code = str.charCodeAt(i);
+      if (
+        !(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)
+      ) {
+        // lower alpha (a-z)
+        return false;
+      }
+    }
+    return true;
+  }
+  // Alphanumeric + dot check function
+  function isAlphaNumericAndDot(username) {
+    var expr = /^[a-zA-Z0-9.]*$/;
+    if (!expr.test(username)) {
+      return false;
+    }
+    return true;
+  }
+  // isUrl function
+  function ValidURL(str) {
+    var regex =
+      /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    if (!regex.test(str)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  let srNo = 1;
   if (globalIsSubmit) {
-    if (firstName.length >= 2) {
+    if (firstName.length >= 3 && isAlphaNumeric(firstName)) {
       document.getElementById("first-name-valid").style.display = "block";
       document.getElementById("first-name-invalid").style.display = "none";
     } else {
@@ -25,7 +75,7 @@ function validate(isSubmit) {
       document.getElementById("first-name-valid").style.display = "none";
     }
 
-    if (lastName.length >= 2) {
+    if (lastName.length >= 3 && isAlphaNumeric(lastName)) {
       document.getElementById("last-name-valid").style.display = "block";
       document.getElementById("last-name-invalid").style.display = "none";
     } else {
@@ -34,25 +84,7 @@ function validate(isSubmit) {
       document.getElementById("last-name-valid").style.display = "none";
     }
 
-    // alphanumeric check function
-    function isAlphaNumeric(str) {
-      var code, i, len;
-
-      for (i = 0, len = str.length; i < len; i++) {
-        code = str.charCodeAt(i);
-        if (
-          !(code > 47 && code < 58) && // numeric (0-9)
-          !(code > 64 && code < 91) && // upper alpha (A-Z)
-          !(code > 96 && code < 123)
-        ) {
-          // lower alpha (a-z)
-          return false;
-        }
-      }
-      return true;
-    }
-
-    if (isAlphaNumeric(username) && username.length > 0) {
+    if (isAlphaNumericAndDot(username) && username.length > 0) {
       document.getElementById("username-valid").style.display = "block";
       document.getElementById("username-invalid").style.display = "none";
     } else {
@@ -74,8 +106,15 @@ function validate(isSubmit) {
       document.getElementById("email-valid").style.display = "none";
     }
 
-    /* City: At least 3 characters */
-
+    if (address.length > 0) {
+      document.getElementById("address-valid").style.display = "block";
+      document.getElementById("address-invalid").style.display = "none";
+    } else {
+      isSubmit = false;
+      document.getElementById("address-invalid").style.display = "block";
+      document.getElementById("address-valid").style.display = "none";
+    }
+    //
     if (phone.match(/^\d{3}-\d{3}-\d{4}$/)) {
       document.getElementById("phone-valid").style.display = "block";
       document.getElementById("phone-invalid").style.display = "none";
@@ -84,6 +123,144 @@ function validate(isSubmit) {
       document.getElementById("phone-invalid").style.display = "block";
       document.getElementById("phone-valid").style.display = "none";
     }
+    if (!ValidURL(website)) {
+      isSubmit = false;
+      document.getElementById("website-invalid").style.display = "block";
+    } else {
+      document.getElementById("website-invalid").style.display = "none";
+    }
   }
-  if (isSubmit) alert("submitted");
+  // Fetch Api
+
+  const url = "https://jsonplaceholder.typicode.com/users";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (isSubmit) {
+        document.querySelector("table").innerHTML = `
+        <thead class="thead-dark table-head">
+        <tr>
+            <th scope="row">Sr No</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Company Name</th>
+        </tr>
+    </thead>
+ <tbody>
+ <tr>
+ <th scope="row">${srNo}</th>
+ <td>${firstName} ${lastName}</td>
+ <td>${username}</td>
+ <td>${email}</td>
+ <td>${address}</td>
+ <td>${phone}</td>
+ <td>${website}</td>
+ <td>${company}</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>${data[0].name}</td>
+<td>${data[0].username}</td>
+<td>${data[0].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[0].phone}</td>
+<td>${data[0].website}</td>
+<td>${data[0].company.name}</td>
+</tr>
+<tr>
+<td>3</td>
+<td>${data[1].name}</td>
+<td>${data[1].username}</td>
+<td>${data[1].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[1].phone}</td>
+<td>${data[1].website}</td>
+<td>${data[1].company.name}</td>
+</tr>
+<td>4</td>
+<td>${data[2].name}</td>
+<td>${data[2].username}</td>
+<td>${data[2].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[2].phone}</td>
+<td>${data[2].website}</td>
+<td>${data[2].company.name}</td>
+</tr>
+</tr>
+<td>5</td>
+<td>${data[3].name}</td>
+<td>${data[3].username}</td>
+<td>${data[3].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[3].phone}</td>
+<td>${data[3].website}</td>
+<td>${data[3].company.name}</td>
+</tr>
+<td>6</td>
+<td>${data[4].name}</td>
+<td>${data[4].username}</td>
+<td>${data[4].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[4].phone}</td>
+<td>${data[4].website}</td>
+<td>${data[4].company.name}</td>
+</tr>
+<td>7</td>
+<td>${data[5].name}</td>
+<td>${data[5].username}</td>
+<td>${data[5].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[5].phone}</td>
+<td>${data[5].website}</td>
+<td>${data[5].company.name}</td>
+</tr>
+<td>8</td>
+<td>${data[6].name}</td>
+<td>${data[6].username}</td>
+<td>${data[6].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[6].phone}</td>
+<td>${data[6].website}</td>
+<td>${data[6].company.name}</td>
+</tr>
+</tr>
+<td>9</td>
+<td>${data[7].name}</td>
+<td>${data[7].username}</td>
+<td>${data[7].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[7].phone}</td>
+<td>${data[7].website}</td>
+<td>${data[7].company.name}</td>
+</tr>
+</tr>
+<td>10</td>
+<td>${data[8].name}</td>
+<td>${data[8].username}</td>
+<td>${data[8].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[8].phone}</td>
+<td>${data[8].website}</td>
+<td>${data[8].company.name}</td>
+</tr>
+<td>11</td>
+<td>${data[9].name}</td>
+<td>${data[9].username}</td>
+<td>${data[9].email}</td>
+<td class="col-3">${data[0].address.street}, ${data[0].address.suite}, ${data[0].address.city}, ${data[0].address.zipcode}</td>
+<td>${data[9].phone}</td>
+<td>${data[9].website}</td>
+<td>${data[9].company.name}</td>
+</tr>
+ </tbody>
+`;
+        resetFields();
+      }
+    });
 }
